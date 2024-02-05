518e40cda2ca3583e622b1b87a402e61c6e3858e
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2024-02-05 23:49:54 +0530

    fixing  favicon issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,32 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x3
+2.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x1
+6.png"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
